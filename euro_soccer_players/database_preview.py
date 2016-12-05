2dd4b327003cd1b5dd8ca5772d378a7791117889
Keyan(Kyle) Yang <kxy156@case.edu>
2016-12-04 22:52:59 -0500

    Create database_preview.py

---
+++ euro_soccer_players/database_preview.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+import pandas as pd
+engine = create_engine('sqlite:///database.sqlite')
+
+table_names = engine.table_names()
+print(table_names)
+
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Player_Attributes')
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+#print(df.columns.values)
+#print(df.head())
+
+df['date'] = pd.to_datetime(df['date'])
+print(type(df[['date']]))
+
+
